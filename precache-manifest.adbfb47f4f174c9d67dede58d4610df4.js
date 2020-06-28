c47c2c926014a6555934ddc9ede966b9dc0d73d6
Wyfy0107 <nguyenduy010798@gmail.com>
2020-06-28 21:36:14 +0300

    Updates

---
+++ precache-manifest.adbfb47f4f174c9d67dede58d4610df4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f435911a8e7f6477c41de814e4659060",
+    "url": "/memory-card-game/index.html"
+  },
+  {
+    "revision": "adb85770aca15734f78a",
+    "url": "/memory-card-game/static/css/main.b5212d32.chunk.css"
+  },
+  {
+    "revision": "61bb047572d1c92999ba",
+    "url": "/memory-card-game/static/js/2.45fa6881.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memory-card-game/static/js/2.45fa6881.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb85770aca15734f78a",
+    "url": "/memory-card-game/static/js/main.0d4a5bba.chunk.js"
+  },
+  {
+    "revision": "5d50e1e2286f7ed45e94",
+    "url": "/memory-card-game/static/js/runtime-main.bc18ae94.js"
+  }
+]);
